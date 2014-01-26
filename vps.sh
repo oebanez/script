82e8eca663b7e2e7c167d24128b7da7f6fbeb5af
oebanez <oebanez@hotmail.com>
2014-01-26 14:08:35 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -4,9 +4,9 @@ rpm -U webmin-1.660-1.noarch.rpm
 
 wget http://dl.fedoraproject.org/pub/epel/5/i386/dropbear-0.55-1.el5.i386.rpm
 rpm -Uvh dropbear-0.55-1.el5.i386.rpm
-wget -O /etc/init.d/dropbear http://newradix.net/repo/dropbear
+wget -O /etc/init.d/dropbear https://raw.github.com/oebanez/script/master/dropbear
 
-wget -O /etc/ssh/sshd_config http://newradix.net/repo/sshd_config
+wget -O /etc/ssh/sshd_config https://raw.github.com/oebanez/script/master/sshd_config
 service sshd restart
 
 service dropbear start
