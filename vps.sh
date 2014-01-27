c0d3d4f87f6e78d307e1b19f1b7b39c16c67a6e1
oebanez <oebanez@hotmail.com>
2014-01-28 00:11:49 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,6 +1,6 @@
 ##Newradix
-wget http://prdownloads.sourceforge.net/webadmin/webmin-1.660-1.noarch.rpm
-rpm -U webmin-1.660-1.noarch.rpm
+wget http://prdownloads.sourceforge.net/webadmin/webmin-1.670-1.noarch.rpm
+rpm -U webmin-1.670-1.noarch.rpm
 
 wget http://dl.fedoraproject.org/pub/epel/5/i386/dropbear-0.55-1.el5.i386.rpm
 rpm -Uvh dropbear-0.55-1.el5.i386.rpm
@@ -24,8 +24,6 @@ rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum -y install fail2ban
 service fail2ban restart
 chkconfig fail2ban on
-
-yum install nethogs -y
 
 wget https://raw.github.com/oebanez/script/master/speedtest-cli.py
 
