372dd1ff9d5bcefdb589e67f467c6c98fba8e88d
oebanez <oebanez@hotmail.com>
2014-01-28 01:01:31 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,6 +1,10 @@
 ##Newradix
+cd
 wget http://prdownloads.sourceforge.net/webadmin/webmin-1.670-1.noarch.rpm
-rpm -U webmin-1.670-1.noarch.rpm
+rpm -i webmin-1.670-1.noarch.rpm;
+rm webmin-1.670-1.noarch.rpm
+service webmin restart
+chkconfig webmin on
 
 wget http://dl.fedoraproject.org/pub/epel/5/i386/dropbear-0.55-1.el5.i386.rpm
 rpm -Uvh dropbear-0.55-1.el5.i386.rpm
